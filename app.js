aa0e20548996b24c111d4a78d440bfd1de1caf8d
Soliperso <chah762002@yahoo.fr>
2022-01-30 09:34:08 -0800

    add active class for the playing video

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let videoList = document.querySelectorAll('.video-list__container .list');
 
 videoList.forEach((video) => {
-  video.onclick = (e) => {
+  video.addEventListener('click', (e) => {
+    videoList.forEach((remove) => {
+      remove.classList.remove('active');
+    });
+    video.classList.add('active');
     let src = video.querySelector('.list-video').src;
     let title = video.querySelector('.list-title').innerHTML;
-
     document.querySelector('.main-video__container .main-video').src = src;
     document.querySelector('.main-video__container .main-video').play();
     document.querySelector('.main-video__container .main-video__title').innerHTML = title;
-  };
+  });
 });
